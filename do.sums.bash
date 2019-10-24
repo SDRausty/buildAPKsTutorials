509c6a789b9b1eb32b4e41cd45e61b5d40ae87cd
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-24 06:05:44 -0400

    modified:   do.sums.bash 	modified:   ma.bash 	renamed:    ztree.sha512.sum -> sha512.sum

--- do.sums.bash
+++ do.sums.bash
@@ -2,31 +2,31 @@
 # Copyright 2019 (c) all rights reserved by S D Rausty; see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
 # To create checksum files and commit use; ./do.sums.bash
-# To see file tree use; awk '{print $2}' ztree.sha512.sum
-# To check the files use; sha512sum -c ztree.sha512.sum
+# To see file tree use; awk '{print $2}' sha512.sum
+# To check the files use; sha512sum -c sha512.sum
 #####################################################################
 set -eu
+git pull
 rm -f *.sum
-FILELIST=( $(find . -type f | grep -vw .git | sort) )
-CHECKLIST=(sha512sum)
+FILELIST=( $(find . -type f | grep -v .git | sort) )
+CHECKLIST=(sha512sum) # md5sum sha1sum sha224sum sha256sum sha384sum
 for SCHECK in ${CHECKLIST[@]}
 do
- 	printf "%s\\n" "Creating $SCHECK file: Please wait a moment..."
+	printf "\\e[1;38;5;118m%s\\e[0m\\n" "Creating $SCHECK file..."
 	for FILE in "${FILELIST[@]}"
 	do
-		$SCHECK "$FILE" >> ztree.${SCHECK::-3}.sum
+		$SCHECK "$FILE" >> ${SCHECK::-3}.sum
 	done
-	chmod 444 ztree.${SCHECK::-3}.sum
 done
+chmod 400 ${SCHECK::-3}.sum 
 for SCHECK in  ${CHECKLIST[@]}
 do
-	printf  "\\n%s\\n" "Checking $SCHECK..."
-	$SCHECK -c ztree.${SCHECK::-3}.sum
+	printf "\\e[1;38;5;119m%s\\e[0m\\n" "Checking $SCHECK..."
+	$SCHECK -c ${SCHECK::-3}.sum
 done
-git pull
 git add .
 git commit
 git push
 ls
-printf "\\e[1;38;5;112m%s\\e[0m\\n" "$PWD"
+printf "\\e[1;38;5;120m%s\\e[0m\\n" "$PWD"
 # do.sums.sh EOF
--- ma.bash
+++ ma.bash
@@ -4,37 +4,9 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SMATRPERROR_() { # run on script error
-	local RV="$?"
-	echo "$RV" ma.bash
-	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
-	exit 147
-}
-
-_SMATRPEXIT_() { # run on exit
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SMATRPSIGNAL_() { # run on signal
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "ma.bash" "$RV"
- 	exit 148 
-}
-
-_SMATRPQUIT_() { # run on quit
-        printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs nma.bash WARNING:  Quit signal %s received near or at line number %s by \`%s\`!\\e[0m\\n" "${1:-UNDEFINED}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
- 	exit 149 
-}
-
-trap '_SMATRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
-trap _SMATRPEXIT_ EXIT
-trap _SMATRPSIGNAL_ HUP INT TERM 
-trap '_SMATRPQUIT_ $? $LINENO $BASH_COMMAND' QUIT 
-
+. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
+# _AT_ username/repository commit
 _AT_ aporter/coursera-android 157373885fbfa18b83fa97cd46f6a003905970ea
 # _AT_ Frank-Zhu/coursera-android a4c1fceac0d2c0a86f74ebad8ccc7539330e8290
 # ma.bash OEF 
