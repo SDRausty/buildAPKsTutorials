b207f94f32530f6dc66b0e7feec5cdc7b56e8d94
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-18 17:36:08 -0500

    commit 365998273 on 20220118

--- ma.bash
+++ ma.bash
@@ -1,5 +1,5 @@
 #!/bin/env bash
-# Copyright 2019 (c) all rights reserved
+# Copyright 2019-2022 (c) all rights reserved
 # by BuildAPKs https://buildapks.github.io
 #####################################################################
 set -Eeuo pipefail
